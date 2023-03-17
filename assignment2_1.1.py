a262422034bd793569d6a5026167e6b775ac4b96
Martin Iversen <martiiv@stud.ntnu.no>
2023-03-17 11:33:02 +0100

    Finished task 1.1

---
+++ assignment2_1.1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib as mp
+import matplotlib.pyplot as plt
+
+#Task 1.1
+#Function for defining mathematical function 
+def fitnessFunction(x, y):
+    return ((x+2*y-7)**2)+((2*x+y-5)**2)            #Defines the mathematical function we are working with (Optimizing)
+
+plt.rcParams["figure.figsize"] = [10, 5]            #Defines the figure    
+plt.rcParams["figure.autolayout"] = True
+
+x = np.linspace(-10,10,200)                         #Defines the range of x values
+y = np.linspace(-10,10,200)                         #Defines the range of y values
+
+plt.plot(x, fitnessFunction(x,y), color='green')    #Plot the function 
+plt.show()
