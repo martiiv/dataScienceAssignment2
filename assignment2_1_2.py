ef43e9c8b3ffea01f162400c19604f1e3c188250
Martin Iversen <martiiv@stud.ntnu.no>
2023-03-20 10:37:09 +0100

    Working on GA

--- assignment2_1_2.py
+++ assignment2_1_2.py
@@ -15,6 +15,30 @@ def printStartOfAlgorithm(populationSize, crossoverProb, mutationProb, numberOfG
     print("Probability of mutation within new gene: "+str(mutationProb*100)+"%, Number of generation for algorithm: "+str(numberOfGen)+ "\n")
     print("____________________________________________________________________________________________________"+" \n")
 
+# Function for applying roulette wheel selection
+# Uses formula 1/result+1 from tutorial 3
+def rouletteWheelSelection(populationSize, functionResults):
+     # For this GA we will be using Roulette Wheel Selection (inspired by tutorial 3)   
+    # We will be using the values from the results list to compute sum of inverted values iValue = inverted value
+    iValues = []
+    for i in range(populationSize):                         #For full range of parents
+        iValue = (1/(functionResults[i]+1))                         #We apply formula 1/(value+1)
+        iValues.append(iValue)                             
+    sumOfInvertedValues = sum(iValues)                      # We sum our results
+
+    #We will now find probability of selecting hypothesis: 
+    probabilities = []
+    for i in range(populationSize):
+        probability = iValues[i]/sumOfInvertedValues
+        probabilities.append(probability)
+    return probabilities
+
+def crossover(numParents, numChrom, resultList, probabilities):
+    numPairs = numParents/2
+    
+    
+
+
 def geneticAlgorithm(populationSize, crossoverProb, mutationProb, numberOfGen): #? We start by defining the main parameters we will use for the GA 
     printStartOfAlgorithm(populationSize, crossoverProb, mutationProb, numberOfGen)
     
@@ -26,15 +50,21 @@ def geneticAlgorithm(populationSize, crossoverProb, mutationProb, numberOfGen): 
         x = rand.randint(-10,10)                            #Defines the range of x values and picks one random value from the range 
         y = rand.randint(-10,10)                            #Defines the range of y values and picks one random value from the range
         chromosomes.append((x,y))                           #Adds coordinate to list
-        if x<0 and y<0:
-            binChrom.append((f'{x:05b}', f'{y:05b}'))       #Adds coordinates in binary    
+        
+        binaryStringX = f'{x:05b}'                          #We convert to binary 
+        binaryStringY = f'{y:05b}'                          # Since we are working with -10 to 10 values we use 4 binary spaces 
+
+        if x<0 and y<0:                                     
+            binaryStringX = binaryStringX.replace('-','1',1)
+            binaryStringY = binaryStringY.replace('-','1',1)
         elif x<0 and y>0:
-            binChrom.append((f'{x:05b}', f'{y:04b}'))       #Adds coordinates in binary
+            binaryStringX = binaryStringX.replace('-','1',1)
         elif x>0 and y<0:
-            binChrom.append((f'{x:04b}', f'{y:05b}'))       #Adds coordinates in binary
-        else:
-            binChrom.append((f'{x:04b}', f'{y:04b}'))       #Adds coordinates in binary
-    
+            binaryStringY = binaryStringY.replace('-','1',1)
+        
+        binChrom.append((binaryStringX, binaryStringY))       #Adds coordinates in binary Neither is negative
+
+        
     print("List of values: "+str(chromosomes)+" \n")            
     print("List of values in binary"+str(binChrom)+" \n")
     
@@ -44,8 +74,15 @@ def geneticAlgorithm(populationSize, crossoverProb, mutationProb, numberOfGen): 
         yValue = chromosomes[i][1]                          #Defining y value
         value = fitnessFunction(xValue, yValue)             #Computing result of fitness function
         results.append(value)                               
-        
-    print(results)
+    
+    probabilities = rouletteWheelSelection(populationSize, results)
+
+    print("Results from fitness function:")
+    
+    for i in range(populationSize):
+        print("___________________________________________________________________________ \n")
+        print("Result of value: "+str(chromosomes[i])+": "+ str(results[i])+"\n")
+        print("Probability of value: "+str(chromosomes[i])+": "+ str(round(probabilities[i],3))+"\n")
     
         
 populationSize = 4
