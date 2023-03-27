7b4e71e0140028b779c0cf0fd5464ca3735c0c0f
Martin Iversen <martiiv@stud.ntnu.no>
2023-03-27 09:55:30 +0200

    Started work on gradient descent

---
+++ assignment2_1_3.py
@@ -0,0 +1,10 @@
+import matplotlib as mtp
+from matplotlib import pyplot as plt
+import numpy as np 
+
+# Task 1.3 Will implement a simple gradient descent algorithm
+# We will be using the fitnessfunction from assignment2_1_1 (Task 1.1)
+from assignment2_1_1 import fitnessFunction
+
+def gradientDescent():
+    return
--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-
-    print(str(candidates)+"\n")
