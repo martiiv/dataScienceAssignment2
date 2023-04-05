bcb660f30ac13582a87af497f505eca7ca75fa6a
Martin Iversen <martiiv@stud.ntnu.no>
2023-04-05 11:50:40 +0200

    Stuff didnt work starting from scratch

--- assignment2_2_1.py
+++ assignment2_2_1.py
@@ -7,14 +7,3 @@ from sklearn.neighbors import KNeighborsClassifier  #For this portion of the ass
 
 #We read the csv files like in assignment 1:
 trainData = pd.read_csv("train.csv",encoding="UTF-8")    #Read from csv 
-one = trainData["1"]
-two = trainData["2"]
-three = trainData["3"]
-four = trainData["4"]
-five = trainData["5"]
-six = trainData["6"]
-seven = trainData["7"]
-attrClass = trainData["label"]
-
-plt.scatter(one,two, c = attrClass)
-plt.show()
